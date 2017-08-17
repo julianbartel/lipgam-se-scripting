def807930b748fbbda854831060139df4eadf520
Julian Bartel <mail@julian-bartel.net>
2017-08-17 23:11:48 +0200

    initial project

---
+++ test/LipGam.SE.Scripting.Common.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LipGam.SE.Scripting.Common.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
